cd96dbc9a9d3f5fb08b25cc8f2bfb1e1faee8461
rezmes <rezmes@gmail.com>
2024-05-21 17:25:41 +0330

    1

--- tutorials/tutorial-getting-started/helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ tutorials/tutorial-getting-started/helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,4 +1,4 @@
-import { Version } from '@microsoft/sp-core-library';
+//import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
@@ -14,28 +14,44 @@ import * as strings from 'helloWorldStrings';
 import { IHelloWorldWebPartProps } from './IHelloWorldWebPartProps';
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
-
+private _isDarkTheme: boolean = false;
+private _environmentMessage: string = '';
   public render(): void {
+    
     this.domElement.innerHTML = `
-      <div class="${styles.helloWorld}">
-        <div class="${styles.container}">
-          <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
-            <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-              <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${styles.button}">
-                <span class="${styles.label}">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
+<section class="${styles.helloWorld}">
+  <div class="${styles.welcome}">
+    <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+    <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+    <div>${this._environmentMessage}</div>
+  </div>
+  <div>
+    <h3>Welcome to SharePoint Framework!</h3>
+    <div>Web part description: <strong>${escape(this.properties.description)}</strong></div>
+    <div>Web part test: <strong>${escape(this.properties.test)}</strong></div>
+    <div>Loading from: <strong>${escape(this.context.pageContext.web.title)}</strong></div>
+  </div>
+</section>`;
+    // this.domElement.innerHTML = `
+    //   <div class="${styles.helloWorld}">
+    //     <div class="${styles.container}">
+    //       <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
+    //         <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+    //           <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+    //           <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
+    //           <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
+    //           <a href="https://aka.ms/spfx" class="${styles.button}">
+    //             <span class="${styles.label}">Learn more</span>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>`;
   }
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
+  // protected get dataVersion(): Version {
+  //   return Version.parse('1.0');
+  // }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
