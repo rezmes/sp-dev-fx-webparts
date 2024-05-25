d5eb2a7d2960677e5b61ba33e666169dfb1e2153
rezmes <rezmes@gmail.com>
2024-05-25 08:49:34 +0330

    Successful without CSS

--- tutorials/tutorial-getting-started/helloworld-webpart/gulpfile.js
+++ tutorials/tutorial-getting-started/helloworld-webpart/gulpfile.js
@@ -4,3 +4,23 @@ const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 
 build.initialize(gulp);
+// const proxy = require('http-proxy-middleware');
+
+// const apiProxy = proxy('/_api', {
+//   target: 'http://portal/sites/mech',
+//   secure: false,
+//   changeOrigin: true,
+//   headers: {
+//     'Authorization': 'Basic ' + Buffer.from('ipr-co\vradmin:پسورد').toString('base64')
+//   }
+// });
+
+// module.exports = {
+//   configureWebpack: function(config) {
+//     config.devServer = {
+//       proxy: {
+//         '/_api': apiProxy
+//       }
+//     };
+//   }
+// };
--- tutorials/tutorial-getting-started/helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ tutorials/tutorial-getting-started/helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -11,27 +11,91 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './HelloWorld.module.scss';
 import * as strings from 'helloWorldStrings';
+import {
+  SPHttpClient,
+  SPHttpClientResponse
+} from '@microsoft/sp-http';
 import { IHelloWorldWebPartProps } from './IHelloWorldWebPartProps';
+
+
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  Title: string;
+  Id: string;
+}
+
+
+
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 private _isDarkTheme: boolean = false;
 private _environmentMessage: string = '';
-  public render(): void {
+private _renderList(items: ISPList[]): void {
+  let html: string = '';
+  items.forEach((item: ISPList) => {
+    html += `
+  <ul class="${styles.list}">
+    <li class="${styles.listItem}">
+      <span class="ms-font-l">${item.Title}</span>
+    </li>
+  </ul>`;
+  });
+
+  if(this.domElement.querySelector('#spListContainer') != null) {
+    this.domElement.querySelector('#spListContainer')!.innerHTML = html;
+  }
+}
+
+private _renderListAsync(): void {
+  this._getListData()
+    .then((response) => {
+      this._renderList(response.value);
+    })
+    .catch(() => {});
+}
+
+public render(): void {
+
+  this.domElement.innerHTML = `
+  <section class="${styles.helloWorld}>
+    <div class="${styles.welcome}">
+      <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+      <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+      <div>${this._environmentMessage}</div>
+    </div>
+    <div>
+      <h3>Welcome to SharePoint Framework!</h3>
+      <div>Web part description: <strong>${escape(this.properties.description)}</strong></div>
+      <div>Web part test: <strong>${escape(this.properties.test)}</strong></div>
+      <div>Loading from: <strong>${escape(this.context.pageContext.web.title)}</strong></div>
+    </div>
+    <div id="spListContainer" />
+  </section>`;
+  
+  this._renderListAsync();
+
+
+
+
+
     
-    this.domElement.innerHTML = `
-<section class="${styles.helloWorld}">
-  <div class="${styles.welcome}">
-    <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-    <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-    <div>${this._environmentMessage}</div>
-  </div>
-  <div>
-    <h3>Welcome to SharePoint Framework!</h3>
-    <div>Web part description: <strong>${escape(this.properties.description)}</strong></div>
-    <div>Web part test: <strong>${escape(this.properties.test)}</strong></div>
-    <div>Loading from: <strong>${escape(this.context.pageContext.web.title)}</strong></div>
-  </div>
-</section>`;
+//     this.domElement.innerHTML = `
+// <section class="${styles.helloWorld}">
+//   <div class="${styles.welcome}">
+//     <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+//     <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+//     <div>${this._environmentMessage}</div>
+//   </div>
+//   <div>
+//     <h3>Welcome to SharePoint Framework!</h3>
+//     <div>Web part description: <strong>${escape(this.properties.description)}</strong></div>
+//     <div>Web part test: <strong>${escape(this.properties.test)}</strong></div>
+//     <div>Loading from: <strong>${escape(this.context.pageContext.web.title)}</strong></div>
+//   </div>
+// </section>`;
     // this.domElement.innerHTML = `
     //   <div class="${styles.helloWorld}">
     //     <div class="${styles.container}">
@@ -47,11 +111,34 @@ private _environmentMessage: string = '';
     //       </div>
     //     </div>
     //   </div>`;
+
+    
+  }
+
+  private _getListData(): Promise<ISPLists> {
+
+    console.log('Fetching list data from:', `${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false`);// get error
+
+    return this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Data:', data);
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error fetching list data", error);
+        return Promise.resolve({ value: [] });
+      });
   }
 
   // protected get dataVersion(): Version {
   //   return Version.parse('1.0');
   // }
+
+
+
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
